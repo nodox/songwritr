6b2dbf3a7db0ea251f952c0ec357562618d4e933
Matvey Genkin <genkin.matt@gmail.com>
2014-09-21 11:47:29 -0500

    Made a function to allow drawing on the canvas.  The function takes a canvas object, a button object that toggles drawing, and a color.

--- js/songwritrScripts.js
+++ js/songwritrScripts.js
@@ -63,3 +63,122 @@ function generateGrid(canvas, bpm, grouping, color) {
 
 
         }
+function enableDrawing(canvas, drawButton, color) {
+
+            //canvas is canvas object, drawbutton is an html button which toggles drawing, color is a hex string
+            context = canvas.getContext("2d");
+
+            var draw = false;
+
+            var paint = false;
+
+            $(drawButton).mousedown(function() {
+
+                draw = !draw;
+
+            });
+
+            $(canvas).mousedown(function(e) {
+
+                paint = true;
+
+                if (draw & paint) {
+
+                    var mouseX = e.pageX - this.offsetLeft;
+
+                    var mouseY = e.pageY - this.offsetTop;
+
+                    addClick(mouseX, mouseY);
+
+                    redraw();
+
+                }
+
+            });
+
+            $(canvas).mousemove(function(e) {
+
+                if (draw & paint) {
+
+                    var mouseX = e.pageX - this.offsetLeft;
+
+                    var mouseY = e.pageY - this.offsetTop;
+
+                    addClick(mouseX, mouseY, true);
+
+                    redraw();
+
+                }
+
+            });
+
+            $(canvas).mouseup(function(e) {
+
+                paint = false;
+
+                redraw();
+
+            });
+
+            $(canvas).mouseleave(function(e) {
+
+                paint = false;
+
+            });
+
+            var clickX = new Array();
+
+            var clickY = new Array();
+
+            var clickDrag = new Array();
+
+
+            function addClick(x, y, dragging) {
+
+                clickX.push(x);
+
+                clickY.push(y);
+
+                clickDrag.push(dragging);
+
+            }
+
+
+            function redraw() {
+
+
+               
+
+ context.strokeStyle = color || "#000000";
+
+                context.lineJoin = "round";
+
+                context.lineWidth = 1;
+
+
+
+                for (var i = 0; i < clickX.length; i++) {
+
+                    context.beginPath();
+
+                    if (clickDrag[i] && i) {
+
+                        context.moveTo(clickX[i - 1], clickY[i - 1]);
+
+                    } else {
+
+                        context.moveTo(clickX[i] - 1, clickY[i]);
+
+                    }
+
+                    context.lineTo(clickX[i], clickY[i]);
+
+                    context.closePath();
+
+                    context.stroke();
+
+                }
+
+            }
+
+        }
