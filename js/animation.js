dd676646beae12c7e0eb4018e2329dd8656a6cdf
Matt Del Signore <mattdelsig@me.com>
2014-09-21 11:39:16 -0400

    cleaned up commented code

--- js/animation.js
+++ js/animation.js
@@ -30,12 +30,10 @@ function makeText(){
       label.textAlign = "left";
       label.y = 0;
       label.x = 0;
-      /*var coor = {x : initialX, y: initialY}; //makes an object with the coordinates */
       var drag = new createjs.Container();
       drag.x = 30;
       drag.y = initialY;
       initialY += 50;
-    /*  coordinates[i] = coor; //store the coordinates*/
       drag.addChild(label);
 
 
@@ -56,28 +54,9 @@ function makeChords(imgUrl){
   var offset; // make an image of a specified chord
   var img = new Image();
   img.src = imgUrl;
-  img.onload = handleImageLoad;
-
-  /*var bitmap = new createjs.Bitmap(img); //create new image from bitmap
-  bitmap.x = 0;
-  bitmap.y = 0;
-  bitmap.cursor = "pointer"; //makes it so the cursor doesn't change
-
-  var dragImg = new createjs.Container(); //add to draggable container
-  dragImg.x = 10;
-  dragImg.y = 10;
+  img.onload = handleImageLoad; //call other on imaghe load method to deal with issues calling methods before image is laoded
 
 
-  bitmap.on("pressmove",function(evt) {
-    evt.currentTarget.x = evt.stageX;
-    evt.currentTarget.y = evt.stageY;
-    stage.update();
-  });
-
-  dragImg.addChild(bitmap);
-
-  stage.addChild(dragImg);
-  stage.update();*/
 }
 
 function handleImageLoad(event){
@@ -94,18 +73,6 @@ bitmap.on("pressmove",function(evt) {
 				evt.currentTarget.y = evt.stageY;
 				stage.update();
 			});
-  /*//when the shape is clicked, update the offset
-  bitmap.on("mousdown",function(evt){
-    this.offset={x:this.x-evt.stageX, y:this.y-evt.stageY};
-  });
-
-  //when the bitmap is dragged, move it in relatin to the string
-  bitmap.on("pressmove",function(evt) {
-    bitmap.x = evt.stageX+this.offset.x;
-    bitmap.y = evt.stagey+this.offset.y;
-    stage.update();
-  });*/
-
   stage.addChild(cont);
   stage.update();
 }
